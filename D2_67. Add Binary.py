40ac884eb6b4bf1243f2aba7e0b495b5efdb6515
JASMINE101 <jasmineck2001@gmail.com>
2023-06-28 00:08:34 +0530

    Day 2 problems- couldn't solve

---
+++ D2_67. Add Binary.py
@@ -0,0 +1,28 @@
+"""Link-https://leetcode.com/problems/add-binary/
+WRONG ANSWER COULDN'T SOLVE
+Given two binary strings a and b, return their sum as a binary string."""
+
+##CODE--
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        carry=0
+        i=0
+        arr=[]
+        while i<len(a) or i<len(b) or carry==1:
+            if a[i]==b[i]:
+                if a[i]==1:
+                    carry=1
+                    arr.append("0")
+                else:
+                    arr.append("0")
+                    carry=0
+            else:
+                if carry==1:
+                    arr.append("0")
+                    carry=1
+                else:
+                    arr.append("1")
+                    carry=0
+                
+        return arr
+                
---
+++ D2_78. Subsets.py
@@ -0,0 +1,19 @@
+""" Link-https://leetcode.com/problems/subsets/
+WRONG ANSWER COULDN'T SOLVE
+Given an integer array nums of unique elements, return all possible subsets (the power set).
+
+The solution set must not contain duplicate subsets. Return the solution in any order."""
+
+#CODE-
+class Solution:
+    
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        arr=[]
+        arr1=[]
+        arr.append(arr1)
+        for x in range(len(nums)+1):
+            for y in range (x):
+                arr1.append(nums[y])
+        arr.append(arr1)        
+                
+        return arr
